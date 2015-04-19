bd1de03f419a8e93cda01d9a355124dc9730ed11
unknown <codar@codar.eigenhuis.lan>
2015-04-19 14:48:32 +0200

    more nubish javascript trials n errors, but at least useful and almost complete

--- otg2mqtt.js
+++ otg2mqtt.js
@@ -1,10 +1,9 @@
-var com = require("serialport");
-var mqtt = require('mqtt');
-
-var topic, message;
-var previous = [];
-
-var opentherm_ids = {
+var com = require('serialport'),
+    mqtt = require('mqtt'),
+    previous = [],
+    topics = [],
+    client,
+    opentherm_ids = {
         0: "flame_status",
         1: "control_setpoint",
         9: "remote_override_setpoint",
@@ -19,9 +18,8 @@ var opentherm_ids = {
         120: "burner_operation_hours",
         121: "ch_pump_operation_hours",
         123: "dhw_burner_operation_hours"
-};
-
-var opentherm_ids_types = {
+    },
+    opentherm_ids_types = {
         0: "flag8",
         1: "f8.8",
         9: "f8.8",
@@ -36,75 +34,158 @@ var opentherm_ids_types = {
         120: "u16",
         121: "u16",
         123: "u16"
-};
+    };
 
-client = mqtt.createClient(1883, 'mqtt.eigenhuis.lan');
-client.subscribe('/control/otg/#');
+(function () {
+    var convertBase = function (num) {
+        this.from = function (baseFrom) {
+            this.to = function (baseTo) {
+                return parseInt(num, baseFrom).toString(baseTo);
+            };
+            return this;
+        };
+        return this;
+    };
+
+    // binary to decimal
+    this.bin2dec = function (num) {
+        return convertBase(num).from(2).to(10);
+    };
+
+    // binary to hexadecimal
+    this.bin2hex = function (num) {
+        return convertBase(num).from(2).to(16);
+    };
+
+    // decimal to binary
+    this.dec2bin = function (num) {
+        return convertBase(num).from(10).to(2);
+    };
+
+    // decimal to hexadecimal
+    this.dec2hex = function (num) {
+        return convertBase(num).from(10).to(16);
+    };
+
+    // hexadecimal to binary
+    this.hex2bin = function (num) {
+        return convertBase(num).from(16).to(2);
+    };
+
+    // hexadecimal to decimal
+    this.hex2dec = function (num) {
+        return convertBase(num).from(16).to(10);
+    };
+
+    return this;
+})();
 
 var serialPort = new com.SerialPort("/dev/ttyUSB0", {
-        baudrate: 9600,
-        parser: com.parsers.readline('\r\n')
+    baudrate: 9600,
+    parser: com.parsers.readline('\r\n')
 });
 
-serialPort.on('open', function() {
-        console.log('Serial port open');
+//serialPort.on( 'open', function () {
+// console.log( 'Serial port open' );
+//} );
+
+client = mqtt.connect('mqtt://mqtt.eigenhuis.lan', {
+    will: {
+        topic: '/status/otg',
+        payload: 'offline',
+        retain: true
+    }
+});
+client.publish('/log/otg', 'service started');
+client.publish('/status/otg', 'online', {retain: true});
+client.subscribe('/control/otg/#');
+
+client.on('message', function (topic, message) {
+    switch (topic) {
+        case '/control/otg/status':
+            result = 'online';
+            break;
+
+        case '/control/otg/tt':
+            serialPort.write('TT=' + message + '\r\n');
+            result = message;
+            break;
+
+        case '/control/otg/tc':
+            serialPort.write('TC=' + message + '\r\n');
+            result = message;
+            break;
+
+        case '/control/otg/hw':
+            result = message;
+            serialPort.write('HW=' + message + '\r\n');
+            break;
+    }
+
+    client.publish("/log/otg" + topic, result);
 });
 
-client.on('message', function(topic, message) {
-        switch (topic) {
-                case '/control/otg/tt':
-                        serialPort.write('TT=' + message + '\r\n');
-                        break;
+serialPort.on('data', function (data) {
+    // check for OT packets
+    opentherm_target = data.slice(0, 1); // B, T, A, R, E
+    opentherm_type = data.slice(1, 2); //
+    opentherm_id = parseInt(data.slice(3, 5), 16); //
+    opentherm_payload = data.slice(-4); // last 4 chars
 
-                case '/control/otg/tc':
-                        serialPort.write('TC=' + message + '\r\n');
-                        break;
+    //      console.log( data );
 
-                default:
-                        console.log("fallback" + topic + message);
-                        break;
-        }
-});
-
-serialPort.on('data', function(data) {
-        // check for OT packets
-        // console.log(data);
-        opentherm_target = data.slice(0, 1); // B, T, A, R, E
-        opentherm_type = data.slice(1, 2); //
-        opentherm_id = parseInt(data.slice(3, 5), 16); //
-        opentherm_payload = data.slice(-4); // last 4 chars
-
+    if (data.length == 9) {
         //if (opentherm_target == "B" || opentherm_target == "T" || opentherm_target == "A" || opentherm_target == "R" || opentherm_target == "E") {
         if (opentherm_target == "B" || opentherm_target == "T" || opentherm_target == "A") {
-                // if (opentherm_type == "1" || opentherm_type == "4" || opentherm_type == "C" || opentherm_type == "9") {
-                if (opentherm_type == "1" || opentherm_type == "4") {
-                        if (opentherm_id in opentherm_ids) {
-                                topic = opentherm_ids[opentherm_id];
-                                switch (opentherm_ids_types[opentherm_id]) {
-                                        case 'flag8':
-                                                message = parseInt(opentherm_payload, 16).toString(2);
-                                                break;
+            if (opentherm_type == "1" || opentherm_type == "4" || opentherm_type == "C" || opentherm_type == "9") {
+                // if (opentherm_type == "1" || opentherm_type == "4" ) {
+                if (opentherm_id in opentherm_ids) {
+                    topic = '/value/otg/' + opentherm_ids[opentherm_id];
+                    switch (opentherm_ids_types[opentherm_id]) {
+                        case 'flag8':
+                            if (opentherm_target != "A") {
+                                topics[topic] = hex2dec(opentherm_payload);
 
-                                        case 'f8.8':
-                                                message = (parseInt(opentherm_payload, 16) / 256).toFixed(2);
-                                                break;
-
-                                        case 'u16':
-                                                message = parseInt(opentherm_payload, 16);
-                                                break;
+                                if (( topics[topic] & ( 1 << 1 ) ) > 0) {
+                                    topics["/value/otg/flame_status_ch"] = 1;
+                                } else {
+                                    topics["/value/otg/flame_status_ch"] = 0;
                                 }
 
-                                // console.log((previous[topic] + previous[message]));
-                                // console.log((topic+message));
-                                // console.log((topic + message) (previous[topic] + previous[message]));
-                                if ((topic + message) != (previous[topic] + previous[message])) {
-                                        client.publish('/value/otg/' + topic, String(message), {
-                                                retain: true
-                                        });
-                                        previous[topic] = topic;
-                                        previous[message] = message;
+                                if (( topics[topic] & ( 1 << 2 ) ) > 0) {
+                                    topics["/value/otg/flame_status_dhw"] = 1;
+                                } else {
+                                    topics["/value/otg/flame_status_dhw"] = 0;
                                 }
+
+                                if (( topics[topic] & ( 1 << 3 ) ) > 0) {
+                                    topics["/value/otg/flame_status_bit"] = 1;
+                                } else {
+                                    topics["/value/otg/flame_status_bit"] = 0;
+                                }
+                            }
+                            break;
+
+                        case 'f8.8':
+                            topics[topic] = ( parseInt(opentherm_payload, 16) / 256 ).toFixed(2);
+                            break;
+
+                        case 'u16':
+                            topics[topic] = parseInt(opentherm_payload, 16);
+                            break;
+                    }
+
+                    // check for changes that need to be published
+                    for (var value in topics) {
+                        if (topics[value] != previous[value]) {
+                            client.publish(value, String(topics[value]), {
+                                retain: true
+                            });
+                            previous[value] = topics[value];
                         }
+                    }
                 }
+            }
         }
+    }
 });
